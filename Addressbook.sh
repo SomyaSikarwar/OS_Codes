e7324d384bf822ab2937d4b3d4c1ed9cb61c44e2
SomyaSikarwar <118437254+SomyaSikarwar@users.noreply.github.com>
2023-10-27 04:48:32 +0530

    Addressbook.sh

---
+++ Addressbook.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+
+ab="addr.txt"
+
+touch "$ab"
+
+Create(){
+    echo "Enter file name"
+    read ab
+    touch $ab
+
+}
+
+Insert(){
+    echo "Enter file name"
+    read ab 
+    
+    res='ls | grep $ab |wc -w'
+    
+    if [ $res -gt 0 ]
+    then
+    echo "File does not exist"
+    else
+    touch $ab
+    fi
+    
+}
+
+
+
+while [ true ]
+do 
+echo "MENUE"
+echo "1) create"
+echo "2) view"
+echo "3. Insert a Record"
+echo "4. Delete a Record"
+echo "5. Modify a record"
+echo "6. Exit"
+
+echo "Enter your choice"
+read choice
+
+case $choice in 
+1) 
+    echo "Enter file name"
+    read ab
+    touch $ab
+
+;;
+2) 
+    cat $ab
+;;
+3)
+	echo "Enter the name of the person : "
+	read personName
+	echo "Enter the address : "
+	read addr
+	
+	echo "$personName $addr" >> "$ab"
+	
+;;
+
+4)
+	echo "Enter the name of the person for deleting the record : "
+	read personName
+	
+    sed -i "/$personName/d" "$ab"
+		
+		echo "successfully deleted!"
+		cat "$ab"
+		;;
+		
+		5)
+		echo "Enter the name of the person whose record is to be modified : "
+		read personName
+		
+		echo "Enter the new address : "
+		read newAddr
+		
+		sed -i "/$personName/c\ $newAddr" "$ab"
+		;;
+		
+		6)
+		break
+		;;
+		
+		*)
+		echo "Invalid input"
+		;;
+
+esac
+
+done
